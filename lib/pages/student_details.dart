51b38ae5e7d08da4b050a65e4f44fbbf139ffc93
sarath <vssarathc04@gmail.com>
2022-05-20 21:53:54 +0530

    Added stuff to main page after login

--- lib/pages/student_details.dart
+++ lib/pages/student_details.dart
@@ -1,6 +1,16 @@
 import 'package:flutter/material.dart';
+import 'package:vtop_app/pages/student_details_pages/academic_details_page.dart';
+
+import 'package:vtop_app/pages/student_details_pages/academic_summary.dart';
+import 'package:vtop_app/pages/student_details_pages/profile_page.dart';
 
 import '../StudentObject.dart';
+
+Color timeTableColor = const Color.fromRGBO(255, 51, 132, 1);
+Color attendanceColor = const Color.fromRGBO(0, 166, 255, 1);
+Color marksColor = const Color.fromRGBO(0, 207, 169, 1);
+Color marksSummaryColor = const Color.fromRGBO(112, 51, 255, 1);
+Color profileColor = const Color.fromRGBO(255, 87, 51, 1);
 
 class DetailsPage extends StatefulWidget {
   final Student studentDetails;
@@ -10,13 +20,56 @@ class DetailsPage extends StatefulWidget {
 }
 
 class _DetailsPageState extends State<DetailsPage> {
+  int _pageIndex = 3;
+  // widget.studentDetails.profile.name
+
+  final List<BottomNavigationBarItem> _bottomNavigationBarItems = [
+    BottomNavigationBarItem(
+      icon: const Icon(Icons.calendar_month_outlined),
+      label: 'Time Table',
+      backgroundColor: timeTableColor,
+    ),
+    BottomNavigationBarItem(
+      icon: const Icon(Icons.access_time),
+      label: 'Attendance',
+      backgroundColor: attendanceColor,
+    ),
+    BottomNavigationBarItem(
+        icon: const Icon(Icons.school_outlined),
+        label: 'Academics',
+        backgroundColor: marksColor),
+    BottomNavigationBarItem(
+      icon: const Icon(Icons.military_tech_outlined),
+      label: 'Summary',
+      backgroundColor: marksSummaryColor,
+    ),
+    BottomNavigationBarItem(
+      icon: const Icon(Icons.person_outline),
+      label: 'Profile',
+      backgroundColor: profileColor,
+    ),
+  ];
+
   @override
   Widget build(BuildContext context) {
-    print(widget.studentDetails);
+    List<Widget> _pages = [
+      const Center(child: Text('Time Table')),
+      const Center(child: Text('Attendance')),
+      AcademicDetails(acadHist: widget.studentDetails.academicHistory.subjects),
+      AcademicSummary(summary: widget.studentDetails.academicHistory.summary),
+      ProfilePage(studentProfile: widget.studentDetails.profile),
+    ];
 
     return Scaffold(
-      body: Center(
-        child: Text(widget.studentDetails.profile.name),
+      body: _pages.elementAt(_pageIndex),
+      bottomNavigationBar: BottomNavigationBar(
+        items: _bottomNavigationBarItems,
+        currentIndex: _pageIndex,
+        onTap: (index) {
+          setState(() {
+            _pageIndex = index;
+          });
+        },
       ),
     );
   }
