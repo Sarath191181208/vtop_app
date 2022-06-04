a6f11b2b6f783f37e618f4b7100887b52f323a71
sarath <vssarathc04@gmail.com>
2022-06-04 09:25:22 +0530

    refactoring changes

--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -4,6 +4,7 @@ import 'dart:async';
 import '/pages/dialogs.dart';
 import '../Student/fetch_student_details.dart';
 import '../Student/student_object.dart';
+import 'Components/custom_button.dart';
 
 class LoginPage extends StatelessWidget {
   const LoginPage({Key? key}) : super(key: key);
@@ -246,55 +247,5 @@ class _LoginFormState extends State<LoginForm> {
             passwordInput
           ],
         ));
-  }
-}
-
-class CustomButton extends StatelessWidget {
-  final String text;
-  final Function onPressed;
-  final IconData icon;
-  const CustomButton(
-      {Key? key,
-      required this.text,
-      required this.onPressed,
-      required this.icon})
-      : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return Row(
-      children: [
-        TextButton(
-            style: TextButton.styleFrom(
-              backgroundColor: Theme.of(context).primaryColor,
-              shape: RoundedRectangleBorder(
-                borderRadius: BorderRadius.circular(30.0),
-              ),
-            ),
-            onPressed: () => onPressed(),
-            // onPressed: () => Navigator.pushReplacementNamed(context, '/details',
-            //     arguments: get_student_obj()),
-            child: Padding(
-              padding: const EdgeInsets.fromLTRB(20.0, 3.0, 20.0, 3.0),
-              child: Row(
-                crossAxisAlignment: CrossAxisAlignment.center,
-                children: [
-                  Text(
-                    text,
-                    textAlign: TextAlign.start,
-                    style: const TextStyle(color: Colors.white),
-                  ),
-                  Padding(
-                    padding: const EdgeInsets.only(left: 8.0),
-                    child: Icon(
-                      icon,
-                      color: Colors.white,
-                    ),
-                  ),
-                ],
-              ),
-            )),
-      ],
-    );
   }
 }
