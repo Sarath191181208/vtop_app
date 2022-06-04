37cd8019d8d139b9945d94a7b03e9f568efbaa83
sarath <vssarathc04@gmail.com>
2022-06-04 10:35:12 +0530

    removed unused import

--- lib/pages/student_details_pages/academic_details_page.dart
+++ lib/pages/student_details_pages/academic_details_page.dart
@@ -3,7 +3,6 @@ import 'package:flutter/material.dart';
 import 'package:vtop_app/Student/student_object.dart';
 import 'package:vtop_app/apis/theme_manager.dart';
 
-import '../Components/null_page.dart';
 import '../Components/sized_icon.dart';
 import '../Components/text_box_decoration.dart';
 
@@ -59,38 +58,46 @@ class AcademicSummary extends StatelessWidget {
     // return Colors.black;
   }
 
+  Widget _buildAcadSummary(BuildContext context, Color? boxClr) {
+    return Column(
+      children: [
+        for (var summaryKey in summaryKeys)
+          Padding(
+            padding:
+                const EdgeInsets.only(left: 20.0, right: 20.0, bottom: 10.0),
+            child: Container(
+              // grey border bottomm
+              decoration: getTextBoxDecoration(boxClr!),
+              child: ListTile(
+                title: Text(
+                  summaryKey,
+                ),
+                trailing: Text(
+                  summary![summaryKey].toString(),
+                  style: TextStyle(
+                      color: _getGradeCountColor(
+                          summaryKey, summary![summaryKey])),
+                ),
+              ),
+            ),
+          ),
+      ],
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
-    var boxClr = Theme.of(context).extension<MyColors>()!.boxColor;
+    Color? boxClr = Theme.of(context).extension<MyColors>()!.boxColor;
 
-    return (summary == null)
-        ? const NullPage(errorMsg: "No academic Summary Found !")
-        : Column(
-            children: [
-              const SizedIcon(
-                  icon: Icons.military_tech_outlined, text: "Academic Summary"),
-              for (var summaryKey in summaryKeys)
-                Padding(
-                  padding: const EdgeInsets.only(
-                      left: 20.0, right: 20.0, bottom: 10.0),
-                  child: Container(
-                    // grey border bottomm
-                    decoration: getTextBoxDecoration(boxClr!),
-                    child: ListTile(
-                      title: Text(
-                        summaryKey,
-                      ),
-                      trailing: Text(
-                        summary![summaryKey].toString(),
-                        style: TextStyle(
-                            color: _getGradeCountColor(
-                                summaryKey, summary![summaryKey])),
-                      ),
-                    ),
-                  ),
-                ),
-            ],
-          );
+    return Column(
+      children: [
+        const SizedIcon(
+            icon: Icons.military_tech_outlined, text: "Academic Summary"),
+        (summary == null)
+            ? const CustomNotFoundWidget(txt: "No Academic Summary Found")
+            : _buildAcadSummary(context, boxClr),
+      ],
+    );
   }
 }
 
@@ -109,36 +116,75 @@ class AcademicHistoryWidget extends StatelessWidget {
     if (s == "F") return Colors.red;
   }
 
+  Widget _buildAcadHist(BuildContext context, Color? boxClr) {
+    return Column(
+      children: [
+        for (var acadHistEntry in acadHist!.entries)
+          Padding(
+            padding:
+                const EdgeInsets.only(left: 20.0, right: 20.0, bottom: 10.0),
+            child: Container(
+              decoration: getTextBoxDecoration(boxClr!),
+              child: ListTile(
+                title: Text(acadHistEntry.key),
+                trailing: Text(
+                  acadHistEntry.value,
+                  style: TextStyle(color: _getGradeColor(acadHistEntry.value)),
+                ),
+              ),
+            ),
+          ),
+      ],
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    Color? boxClr = Theme.of(context).extension<MyColors>()!.boxColor;
+
+    return Column(
+      children: [
+        const SizedIcon(
+          icon: Icons.school_outlined,
+          text: "Academic History",
+          upperSpacingSize: 50.0,
+        ),
+        (acadHist == null)
+            ? const CustomNotFoundWidget(txt: "No Academic History Found")
+            : _buildAcadHist(context, boxClr)
+      ],
+    );
+  }
+}
+
+class CustomNotFoundWidget extends StatelessWidget {
+  final String txt;
+
+  const CustomNotFoundWidget({Key? key, required this.txt}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
     var boxClr = Theme.of(context).extension<MyColors>()!.boxColor;
 
-    return (acadHist == null)
-        ? const NullPage(errorMsg: "Can't seem to find the academic details")
-        : Column(
-            children: [
-              const SizedIcon(
-                icon: Icons.school_outlined,
-                text: "Academic History",
-                upperSpacingSize: 50.0,
-              ),
-              for (var acadHistEntry in acadHist!.entries)
-                Padding(
-                  padding: const EdgeInsets.only(
-                      left: 20.0, right: 20.0, bottom: 10.0),
-                  child: Container(
-                    decoration: getTextBoxDecoration(boxClr!),
-                    child: ListTile(
-                      title: Text(acadHistEntry.key),
-                      trailing: Text(
-                        acadHistEntry.value,
-                        style: TextStyle(
-                            color: _getGradeColor(acadHistEntry.value)),
-                      ),
-                    ),
-                  ),
+    return Padding(
+      padding: const EdgeInsets.fromLTRB(20, 10, 20, 10),
+      child: Container(
+          decoration: getTextBoxDecoration(boxClr!),
+          child: Padding(
+            padding: const EdgeInsets.fromLTRB(40, 20, 40, 20),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                const Icon(
+                  Icons.error,
+                  color: Colors.red,
+                  size: 40.0,
                 ),
-            ],
-          );
+                const SizedBox(height: 20),
+                Text(txt, style: const TextStyle(color: Colors.grey)),
+              ],
+            ),
+          )),
+    );
   }
 }
